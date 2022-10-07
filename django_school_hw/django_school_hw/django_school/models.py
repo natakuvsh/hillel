32634f0147a90cb6ef03723ecb76554596e8fa33
Natalia Zharikova <natakuvsh@gmail.com>
2022-10-07 20:15:38 +0300

    Added Course model, paginator

--- django_school_hw/django_school_hw/django_school/models.py
+++ django_school_hw/django_school_hw/django_school/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 
 
-class Teacher(models.Model):
+def course_upload_path(obj, file):
+    return f'course/{obj.name}/{file}'
+
+
+class NameAgeEmail(models.Model):
     name = models.CharField(max_length=255)
     age = models.PositiveIntegerField(null=True)
     email = models.EmailField(max_length=255, unique=True)
     group = models.ForeignKey("django_school.Group", on_delete=models.SET_NULL, null=True)
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
-        return f'{self.name},{self.age} years old, {self.email}'
+        return f'{self.name}   {self.email}'
+
+
+class Teacher(NameAgeEmail):
+   pass
+
+
+class Student(NameAgeEmail):
+    pass
 
 
 class Group(models.Model):
@@ -18,11 +33,25 @@ class Group(models.Model):
         return self.name
 
 
-class Student(models.Model):
+class ProductManager(models.Manager):
+
+    def get_queryset(self):
+        queryset = super(ProductManager, self).get_queryset()
+        return queryset.filter(teacher__isnull=False)
+
+
+class Course(models.Model):
     name = models.CharField(max_length=255)
-    age = models.PositiveIntegerField(null=True)
-    email = models.EmailField(max_length=255, unique=True)
-    group = models.ForeignKey("django_school.Group", on_delete=models.SET_NULL, null=True)
+    description = models.TextField()
+    teacher = models.ForeignKey("Teacher", on_delete=models.SET_NULL, null=True, blank=True)
+    theses = models.TextField()
+    group = models.ManyToManyField("Group")
+    image = models.ImageField(upload_to=course_upload_path, null=True)
+
+    objects = ProductManager()
 
     def __str__(self):
-        return f'{self.name},{self.age} years old, {self.email}'
+        return self.name
+
+
+

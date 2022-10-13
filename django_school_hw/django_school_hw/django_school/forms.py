b907c1570ff6e23b3e1c93cde0c91609d6cde5e5
Natalia Zharikova <natakuvsh@gmail.com>
2022-10-13 17:34:30 +0300

    Added student form, course form, search

---
+++ django_school_hw/django_school_hw/django_school/forms.py
@@ -0,0 +1,64 @@
+from django import forms
+from django_school.models import Teacher, Group, Course, Student
+
+
+def check_surname(surname):
+    if Student.objects.filter(surname=surname):
+        raise forms.ValidationError("Surname must be unique")
+    return surname
+
+
+class CourseCreateForm(forms.Form):
+    name = forms.CharField(required=True)
+    teacher = forms.ModelChoiceField(queryset=Teacher.objects.all())
+    description = forms.CharField(widget=forms.widgets.Textarea(attrs={'cols': 50, 'rows': 3}))
+    theses = forms.CharField(widget=forms.widgets.Textarea(attrs={'cols': 50, 'rows': 3}))
+    image = forms.ImageField()
+    group = forms.ModelMultipleChoiceField(
+        queryset=Group.objects.all(),
+        widget=forms.widgets.CheckboxSelectMultiple
+    )
+
+    def create_course(self):
+        course = Course.objects.create(
+            name=self.cleaned_data['name'],
+            teacher=self.cleaned_data['teacher'],
+            description=self.cleaned_data['description'],
+            theses=self.cleaned_data['theses'],
+            image=self.cleaned_data['image'],
+        )
+
+        course.group.add(*self.cleaned_data['group'])
+        return course
+
+
+class StudentCreateForm(forms.Form):
+    name = forms.CharField(strip=True)
+    surname = forms.CharField(required=True, strip=True, validators=[check_surname])
+    age = forms.IntegerField(min_value=18, max_value=130)
+    email = forms.EmailField()
+    group = forms.ModelChoiceField(queryset=Group.objects.all())
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if " " in name:
+            raise forms.ValidationError("Please do not use whitespaces")
+        return name
+
+    def clean_surname(self):
+        surname = self.cleaned_data['surname']
+        if " " in surname:
+            raise forms.ValidationError("Please do not use whitespaces")
+        return surname
+
+    def create_student(self):
+        student = Student.objects.create(
+            name=self.cleaned_data['name'],
+            surname=self.cleaned_data['surname'],
+            age=self.cleaned_data['age'],
+            email=self.cleaned_data['email'],
+            group=self.cleaned_data['group'],
+        )
+
+        return student
+
